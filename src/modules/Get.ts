320d70b379b26a30ef31ae5425584867e450b6a3
Rimfis <dimitri.rim@yandex.ru>
2024-10-21 22:01:41 +0300

    fix: functions  writing and reading to a file removed from Gets module

--- src/modules/Get.ts
+++ src/modules/Get.ts
@@ -1,50 +1,46 @@
 import { ServerResponse } from 'node:http';
-import { readFile } from 'node:fs/promises';
+import { User, usersDB } from '../api/users';
 
-type User = {
-  id: string;
-  username: string;
-  age: number;
-  hobbies: string[];
-};
-
-export const getUsersAll = async (res: ServerResponse) => {
-  const file = await readFile('./src/api/users.json');
+export const getUsersAll = (res: ServerResponse) => {
   res.statusCode = 200;
-  res.end(file);
+  res.end(JSON.stringify(usersDB));
 };
 
-export const getUserById = async (res: ServerResponse, userId: string) => {
-    if (isUuid(res ,userId)) {
-        const userData = await isUserById(res ,userId)
+export const getUserById =  (res: ServerResponse, userId: string) => {
+  if (isUuid(res, userId)) {
+    const userData =  isUserById(res, userId);
 
-        if(userData !== null){
-            res.statusCode = 200;
-            res.end(JSON.stringify(userData));
-        }
+    if (userData !== null) {
+      res.statusCode = 200;
+      res.end(JSON.stringify(userData));
     }
+  }
 };
 
-export const isUuid  = (res: ServerResponse, userId: string): boolean => {
-    if (!/^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$/.test(userId)) {
-        res.statusCode = 400;
-        res.end('not uuid');
-        return false;
-    }
-    return true;
-} 
+export const isUuid = (res: ServerResponse, userId: string): boolean => {
+  if (
+    !/^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$/.test(
+      userId,
+    )
+  ) {
+    res.statusCode = 400;
+    res.end('not uuid');
+    return false;
+  }
+  return true;
+};
 
-export const isUserById = async (res: ServerResponse, userId: string): Promise<User | null> => {
-    const file = await readFile('./src/api/users.json', { encoding: 'utf8' });
-    const user = JSON.parse(file).dataUsers.find(
-      (item: User) => item.id === userId,
-    );
-    console.log(user)
-    if (!user) {
-        res.statusCode = 400;
-        res.end('User not found');
-        return null;
-    }
+export const isUserById = (
+  res: ServerResponse,
+  userId: string,
+): User | null => {
+  const user = usersDB.find((item: User) => item.id === userId);
 
-    return user;
-}
+  if (!user) {
+    res.statusCode = 400;
+    res.end('User not found');
+    return null;
+  }
+
+  return user;
+};
